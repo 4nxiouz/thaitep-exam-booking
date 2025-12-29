74f0257452ab7cbdd771c70ac8a0f6ba0a742842
4nxiouz <zizplenter@gmail.com>
2025-12-29 09:10:39 +0700

    Initialize Supabase client in supabase.ts

---
+++ lip/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://glnubvrpnmmzmtokmohk.supabase.co';
+const supabaseAnonKey = 'sb_publishable_xGEuXdUNLRd63cMqMZP-nw_PA9XA5Ef';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
