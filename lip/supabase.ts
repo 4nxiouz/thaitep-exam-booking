3274f776f8428278f26ee4d5b8d57b4d1b909d80
4nxiouz <zizplenter@gmail.com>
2025-12-29 09:19:55 +0700

    Change Supabase client import to supabase-ts

--- lip/supabase.ts
+++ lip/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-ts';
 
 const supabaseUrl = 'https://glnubvrpnmmzmtokmohk.supabase.co';
 const supabaseAnonKey = 'sb_publishable_xGEuXdUNLRd63cMqMZP-nw_PA9XA5Ef';
